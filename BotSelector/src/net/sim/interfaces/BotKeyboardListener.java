55fc1bb2ebcb6cda7752e5097e3b56a9509aa783
gargoyle3333 <mikeyman@hotmail.co.uk>
2013-05-17 10:12:06 +0100

    Writing project completely from scratch. Deleted everything and put in blank classes for filling.

--- BotSelector/src/net/sim/interfaces/BotKeyboardListener.java
+++
@@ -1,9 +0,0 @@
-package net.sim.interfaces;
-
-public interface BotKeyboardListener {
-	
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void keyTyped(int key);
-
-}
--- BotSelector/src/net/sim/interfaces/BotWindowListener.java
+++
@@ -1,7 +0,0 @@
-package net.sim.interfaces;
-
-public interface BotWindowListener {
-
-	void windowResized(int width, int height);
-	
-}
