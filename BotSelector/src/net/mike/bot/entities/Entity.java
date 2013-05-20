b1d9aded5e98205c58b53666d17826d6b5713858
gargoyle3333 <mikeyman@hotmail.co.uk>
2013-05-20 15:57:39 +0100

    Changed size to a float for collisions - drawing methods need to scale. Added an age variable, known as mFramesAlive.

--- BotSelector/src/net/mike/bot/entities/Entity.java
+++ BotSelector/src/net/mike/bot/entities/Entity.java
@@ -8,11 +8,37 @@ public abstract class Entity {
 	protected State mState;
 	protected Color mColor;
 	protected Vector2f mPosition, mVelocity;
-	protected int mSize;
+	protected int mFramesAlive;
+	protected float mSize;
+	
+	public Vector2f getPosition() {
+		return mPosition;
+	}
+
+	public void setPosition(Vector2f position) {
+		this.mPosition = position;
+	}
+
+	public Vector2f getVelocity() {
+		return mVelocity;
+	}
+
+	public void setVelocity(Vector2f velocity) {
+		this.mVelocity = velocity;
+	}
+
+	public float getSize() {
+		return mSize;
+	}
+
+	public void setSize(float size) {
+		this.mSize = size;
+	}
+	
 	
 	public Entity() {
 		mState = State.ALIVE;
-		mColor = new Color(1, 1, 1);
+		mColor = new Color(255, 255, 255);
 	}
 	
 	public enum State {
